71e238354df1b6fa8d6294365e43036a3e7b4398
scottmotte <scott@scottmotte.com>
2014-07-06 14:02:57 -0700

    ➿

--- create.rb
+++ create.rb
@@ -1,16 +1,17 @@
-# encoding: UTF-8
 require "json"
 require "fileutils"
 
 directory = "emoji"
 
-result = File.open("emoji-bytes.json", "r").read
+result = File.open("emoji.json", "r:UTF-8").read
 emoji = JSON.parse(result)["emoji"]
-emoji.each do |emo|
-  filename = [directory, "/", emo, ".md"].join()
-  #FileUtils.touch(filename) 
-  File.open(filename, "w:UTF-8") do |f|
-      f.write "hi"
-  end
-end
+puts emoji[0].encode("UTF-8")
+# emoji.each do |emo|
+#   print emo #.encode("UTF-8")
+#   filename = [directory, "/", emo, ".md"].join()
+#   #FileUtils.touch(filename) 
+#   #File.open(filename, "w:UTF-8") do |f|
+#   #    f.write "hi"
+#   #end
+# end
 
